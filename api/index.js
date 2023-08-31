22252daca574f240aedc059bcd6692ef378f44f7
siddheshsurve <sysurve24@gmail.com>
2023-08-31 11:07:14 +0530

    register logic

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 const express = require("express")
-const cors = require("cors")
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/User.js");
+require('dotenv').config()
 
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 
@@ -10,13 +16,28 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 
+
+mongoose.connect(process.env.MONGO_URL);
+
 app.get('/test', (req, res) => {
     res.json("test ok");
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async(req, res) => {
     const {name, email, password} = req.body;
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
     
+        res.json(userDoc);
+    } catch(e) {
+        res.status(422).json(e);
+    }
 })
+
+
 
 app.listen(4000);
